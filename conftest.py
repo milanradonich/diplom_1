5c33cac74c0b7d69a3ec09b7ec919f6524d0566f
Milan Radonich <mradonich90@gmail.com>
2024-10-21 23:20:18 +0300

    add test_burger.py, test_ingredient.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,24 @@
 import pytest
 
 from bun import Bun
+from burger import Burger
+from ingredient import Ingredient
 
 
 @pytest.fixture
 def bun_instance():
     """фикстура создание экземпляра класса Bun"""
-    return Bun(name=None, price=None)
+    return Bun(name='Вкусная булочка', price=100.50)
+
+
+@pytest.fixture
+def burger_instance():
+    """фикстура создание экземпляра класса Burger"""
+    return Burger()
+
+
+@pytest.fixture
+def ingredient_instance():
+    """фикстура создание экземпляра класса Bun"""
+    return Ingredient(ingredient_type='Соусы', name='Соус фирменный Space Sauce', price=80.0)
+
